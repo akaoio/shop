015349593a582cbc54820ecb825856f602d68e2e
akao <null>
2025-12-10 14:48:49 +0700

    fix: update server host configuration to use process.env.IP

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import server from "live-server"
 
 server.start({
     port: 8080, // Set the server port. Defaults to 8080.
-    host: "127.0.0.1", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    host: process.env.IP, // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
     root: "build", // Set root directory that's being served. Defaults to cwd.
     open: true, // When false, it won't load your browser by default.
     file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
