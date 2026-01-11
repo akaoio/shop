867012ffe720071a9945ec4e3af940a2cd127e69
x <null>
2026-01-11 19:34:22 +0700

    refactor: Remove OPTION and ITEM components; update styles for item route

--- src/UI/components/option/styles.js
+++
@@ -1,5 +0,0 @@
-import { css } from "/core/UI.js"
-
-export const styles = css``
-
-export default styles
--- src/UI/routes/item/[item]/styles.css.js
+++ src/UI/routes/item/[item]/styles.css.js
@@ -1,14 +1,16 @@
 import { css } from "/core/UI.js"
 
 export const styles = css`
-    #item {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-template-areas:
+    :host {
+        #item {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-areas:
             "image header"
             "image main"
             "image footer";
-        gap: var(--space, 1rem);
+            gap: var(--space, 1rem);
+        }
     }
 `
 
